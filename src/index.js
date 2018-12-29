8289be6183dcafd7c6d9e8e123ac83256a16e782
Steve <steveorsino@gmail.com>
2018-12-29 13:19:41 -0500

    added middleware

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers'
+import reducer from './reducers';
+import middleware from './middleware';
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
