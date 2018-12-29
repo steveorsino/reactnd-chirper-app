2051dc728061e59fe14d2c10f9e8155afe0e425d
Steve <steveorsino@gmail.com>
2018-12-29 16:31:58 -0500

    added tweet and loading bar component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import users from './users';
 import tweets from './tweets';
 import authedUser from './authedUser';
+import { loadingBarReducer } from 'react-redux-loading';
 
 export default combineReducers({
   authedUser,
   users,
-  tweets
+  tweets,
+  loadingBar: loadingBarReducer,
 });
