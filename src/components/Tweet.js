de911b2eec8c0d6684692638a6352b360b0fc8fc
Steve <steveorsino@gmail.com>
2018-12-30 13:31:09 -0500

    added react-router-dom

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,12 +5,13 @@ import { TiArrowBackOutline } from 'react-icons/ti';
 import { TiHeartOutline } from 'react-icons/ti';
 import { TiHeartFullOutline } from 'react-icons/ti';
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom';
 
 class Tweet extends Component {
 
   toParent = (e, id) => {
     e.preventDefault();
-    //TODO: redirect to parent tweet
+    this.props.history.push(`/tweet/${id}`);
   };
 
   handleLike = (e) => {
@@ -32,11 +33,11 @@ class Tweet extends Component {
     }
 
     const {
-      name, avatar, timestamp, text, hasLiked, likes, replies, parent
+      name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
     } = tweet;
 
     return (
-      <div className='tweet'>
+      <Link to={`/tweet/${id}`} className='tweet'>
         <img
           src={avatar}
           alt={`Avatar of ${name}`}
@@ -67,7 +68,7 @@ class Tweet extends Component {
             <span>{likes !== 0 && likes}</span>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
@@ -84,4 +85,4 @@ function mapStateToProps ({authedUser, users, tweets}, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet));
